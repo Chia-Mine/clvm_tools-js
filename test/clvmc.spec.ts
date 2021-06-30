e4861c44b119de8969d59ee81e17b0d46f28ba3b
ChiaMineJP <admin@chiamine.jp>
2021-07-01 00:18:27 +0900

    Added clvmc test

--- test/clvmc.spec.ts
+++ test/clvmc.spec.ts
@@ -15,6 +15,12 @@ const EXPECTED_HEX_OUTPUT = "ff10ff02ffff0182177180"
 
 // # `EXPECTED_HEX_OUTPUT` disassembles to "(+ 2 (q . 6001))"
 
+afterAll(() => {
+  const mainSymPath = path.resolve(__dirname, "..", "main.sym");
+  if(fs.existsSync(mainSymPath)){
+    fs.unlinkSync(mainSymPath);
+  }
+});
 
 test("test_compile_clvm_text", (done) => {
   tmp.dir({unsafeCleanup: true}, function tempFileCreated(err, include_dir, cleanupCallback){
@@ -35,5 +41,41 @@ test("test_compile_clvm_text", (done) => {
     finally {
       cleanupCallback();
     }
+  });
+});
+
+test("test_compile_clvm", (done) => {
+  tmp.dir({unsafeCleanup: true}, function tempDirCreated(err, include_dir, cleanupCallback){
+    tmp.dir({unsafeCleanup: true}, function tempDir2Created(err2, source_dir, cleanupCallback2){
+      try{
+        if(err){
+          return done(err);
+        }
+        if(err2){
+          return done(err2);
+        }
+        const include_path = path.resolve(include_dir, "include.clvm");
+        fs.writeFileSync(include_path, INCLUDE_CODE);
+        
+        const main_path = path.resolve(source_dir, "main.clvm");
+        const main_output = path.resolve(source_dir, "main.hex");
+        fs.writeFileSync(main_path, MAIN_CODE);
+        
+        const output = clvmc.compile_clvm(
+          main_path, main_output, [include_dir],
+        );
+  
+        const t = fs.readFileSync(output, {encoding: "utf8"});
+        expect(t).toBe(`${EXPECTED_HEX_OUTPUT}\n`);
+        done();
+      }
+      catch(e){
+        done(e);
+      }
+      finally {
+        cleanupCallback();
+        cleanupCallback2();
+      }
+    });
   });
 });
