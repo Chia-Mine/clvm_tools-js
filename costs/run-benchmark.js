3a5727b8bd2b6ed2d213d31bd75e24d92160951c
ChiaMineJP <admin@chiamine.jp>
2021-08-12 00:36:58 +0900

    Added -o/-n options

--- costs/run-benchmark.js
+++ costs/run-benchmark.js
@@ -9,6 +9,8 @@ let open_files = {};
 let counter = 0;
 let force_run = false;
 let grepString = "";
+let only_heaviest = false;
+let number_of_iterations = 1;
 
 function toPosixPath(p){
   return p.replace(/\\/g, "/");
@@ -33,7 +35,7 @@ function get_file(folder, name, dry_run){
     fd = false;
   }
   else {
-    fd = fs.openSync(full_path, "a");
+    fd = fs.openSync(full_path, "w");
   }
   open_files[posix_full_path] = fd;
   
@@ -41,8 +43,50 @@ function get_file(folder, name, dry_run){
     return fd;
   }
   
-  fs.writeSync(fd, "#cost,assemble_from_ir,to_sexp_f,run_program,multiplier\n");
+  fs.writeSync(fd, "#cost,assemble_from_ir,to_sexp_f,run_program,multiplier,n\n");
   return fd;
+}
+
+function find_heaviest_benchmark_files(directory){
+  const is_apply_bench = path.basename(directory) === "apply";
+  const clvm_files = glob.sync(toPosixPath(path.resolve(directory, "*.clvm")));
+  if(is_apply_bench){
+    return clvm_files[0];
+  }
+  
+  const regex = /(.+)-([0-9]+)-([0-9]+)\.clvm/;
+  const obj = {};
+  let max_bytes = 0;
+  let max_n = 0;
+  let heaviest_i = -1;
+  for(let i=0;i<clvm_files.length;i++){
+    const fn = clvm_files[i];
+    const filename = path.basename(fn);
+    const match = regex.exec(filename);
+    if(!match || match.length < 3 || isNaN(+match[2]) || isNaN(+match[3])){
+      continue;
+    }
+    const name = match[1];
+    const bytes = +match[2];
+    const n = +match[3];
+    if(!obj[name]){
+      obj[name] = {
+        max_bytes: bytes,
+        max_n: n,
+        heaviest_i: i,
+      };
+    }
+    else if(bytes >= obj[name].max_bytes && n >= obj[name].max_n){
+      obj[name].max_bytes = bytes;
+      obj[name].max_n = n;
+      obj[name].heaviest_i = i;
+    }
+  }
+  
+  const heaviest_i_all = Object.keys(obj).map(k => obj[k].heaviest_i);
+  return clvm_files.filter((_, i) => {
+    return heaviest_i_all.includes(i);
+  });
 }
 
 function run_benchmark_file(fn, existing_results){
@@ -53,60 +97,59 @@ function run_benchmark_file(fn, existing_results){
     return;
   }
   
-  // if we have a csv file for this run already, skip running it again
-  const dry_run = !force_run && path.basename(fn).split('-')[0] in existing_results;
-  
-  if(!dry_run){
-    console.log(`${counter.toString().padStart(4, '0')}: ${toOSPath(fn)}`);
-  }
   counter += 1;
   
+  // if we have a csv file for this run already, skip running it again
+  const dry_run = !force_run && path.basename(fn).split('-')[0] in existing_results;
+  if(dry_run){
+    return;
+  }
+  
+  console.log(`${counter.toString().padStart(4, '0')}: ${toOSPath(fn)}`);
   const counters = {};
   // the filename is expected to be in the form:
   // name "-" value_size "-" num_calls
-  if(!dry_run){
-    const env = fs.readFileSync(fn.replace(/\.clvm$/, ".env"));
-    const commandPath = path.resolve(__dirname, "..", ".dist", "npm", "bin", "cli.js");
-    if(!fs.existsSync(commandPath)){
-      console.error(`You need to build clvm_tools first. Executable not found at: ${commandPath}`);
-      process.exit(1);
-      return;
-    }
-    const command = `node "${commandPath}" brun -c --quiet --time "${toOSPath(fn)}" "${env}"`;
+  const env = fs.readFileSync(fn.replace(/\.clvm$/, ".env"));
+  const commandPath = path.resolve(__dirname, "..", ".dist", "npm", "bin", "cli.js");
+  if(!fs.existsSync(commandPath)){
+    console.error(`You need to build clvm_tools first. Executable not found at: ${commandPath}`);
+    process.exit(1);
+    return;
+  }
+  
+  const command = `node "${commandPath}" brun -c --quiet --time "${toOSPath(fn)}" "${env}"`;
+  for(let i=0;i<number_of_iterations;i++){
     const outputBuffer = child_process.execSync(command);
     const outputLines = outputBuffer.toString("ascii").split(/\n/g, 5);
     const output = outputLines.slice(0, outputLines.length-1);
-    
+  
+    const regex = /^(.+)[:=](.+)$/;
     for(const o of output){
-      try{
-        if(o.includes(":")){
-          const [key, value] = o.split(":");
-          counters[key.trim()] = value.trim();
-        }
-        else if(o.includes("=")){
-          const [key, value] = o.split("=");
-          counters[key.trim()] = value.trim();
-        }
-      }
-      catch(e){
-        console.error(e);
+      const matcher = regex.exec(o);
+      if(!matcher){
         console.error(`ERROR parsing: ${o}`);
+        continue;
       }
+      const key = matcher[1].trim();
+      const value = +matcher[2].trim();
+      counters[key] = (counters[key] || 0) + value;
     }
-    console.log(JSON.stringify(counters));
   }
+  for(const name in counters){
+    counters[name] /= number_of_iterations;
+  }
+  console.log(JSON.stringify(counters));
   
   const name_components = filename.split("-");
   const fd = get_file(folder, name_components.slice(0, name_components.length-1).join("-"), dry_run);
-  if(!dry_run){
-    const line = counters["cost"] + ","
-      + counters["assemble_from_ir"] + ","
-      + counters["to_sexp_f"] + ","
-      + counters["run_program"] + ","
-      + name_components[name_components.length-1].split(".")[0]
-      + "\n";
-    fs.writeSync(fd, line); // `fd` will be closed later
-  }
+  const line = counters["cost"] + ","
+    + counters["assemble_from_ir"] + ","
+    + counters["to_sexp_f"] + ","
+    + counters["run_program"] + ","
+    + name_components[name_components.length-1].split(".")[0] + ","
+    + number_of_iterations
+    + "\n";
+  fs.writeSync(fd, line); // `fd` will be closed later
 }
 
 function run_benchmark_folder(directory){
@@ -115,7 +158,11 @@ function run_benchmark_folder(directory){
     existing_results.push(path.basename(r).split('-')[1]);
   }
   
+  const heaviest_benchmark_fn = find_heaviest_benchmark_files(directory);
   for(const fn of glob.sync(toPosixPath(path.resolve(directory, "*.clvm")))){
+    if(only_heaviest && !heaviest_benchmark_fn.includes(fn)){
+      continue;
+    }
     run_benchmark_file(fn, existing_results);
   }
   
@@ -149,6 +196,14 @@ function main(){
     ["-f", "--force"], {action: "store_true",
       help: "Run regardless result file exists"},
   );
+  parser.add_argument(
+    ["-o", "--only-heaviest"], {action: "store_true",
+      help: "Run only the heaviest benchmark for each benchmark type"},
+  );
+  parser.add_argument(
+    ["-n", "--number-of-try"], {type: "int", default: 1,
+      help: "Number of benchmark iterations for accurate benchmark result"},
+  );
   
   const parsedArgs = parser.parse_args(process.argv.slice(2));
   if(parsedArgs.root_dir){
@@ -160,6 +215,10 @@ function main(){
   if(parsedArgs.grep){
     grepString = parsedArgs.grep;
   }
+  if(parsedArgs.only_heaviest){
+    only_heaviest = true;
+  }
+  number_of_iterations = parsedArgs.number_of_try;
   
   run_benchmark_all(benchmarkRoot);
 }
